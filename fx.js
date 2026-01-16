5a316d482163f36c0b33355bac8b9410ce9f2f6e
dooseobshin <dooseob.shin@gmail.com>
2026-01-16 11:50:29 +0900

    Delete fx.js

--- fx.js
+++
@@ -1,5 +0,0 @@
-export default async function handler(req, res) {
-  const r = await fetch('https://open.er-api.com/v6/latest/USD');
-  const data = await r.json();
-  res.status(200).json({ rate: data.rates.KRW });
-}
